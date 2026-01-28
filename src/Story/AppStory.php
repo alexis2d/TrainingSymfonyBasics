1964b57d9f831190ab2980d7ae80406555dce85c
Alexis Duquenet <alexisduquenet@gmail.com>
2026-01-28 15:38:02 +0100

    Fix story

--- src/Story/AppStory.php
+++ src/Story/AppStory.php
@@ -2,6 +2,7 @@
 
 namespace App\Story;
 
+use App\Factory\ConferenceFactory;
 use Zenstruck\Foundry\Attribute\AsFixture;
 use Zenstruck\Foundry\Story;
 
@@ -10,6 +11,6 @@ final class AppStory extends Story
 {
     public function build(): void
     {
-        // SomeFactory::createOne();
+        ConferenceFactory::createMany(20);
     }
 }
